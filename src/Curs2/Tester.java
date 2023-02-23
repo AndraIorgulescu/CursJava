8c18f02aaa4172ac85260be3d8ebb79659b5f7cf
Andra Iorgulescu <andravoicav@gmail.com>
2023-02-23 21:44:19 +0200

    Curs2

---
+++ src/Curs2/Tester.java
@@ -0,0 +1,29 @@
+package Curs2;
+
+public class Tester{
+	
+	public Tester() {
+		
+		
+	}
+	String name;
+	int ratePerHour;
+	
+	//metode
+	public String getName() {
+		return name;
+	}
+	public void setName(String parametru) {
+		name = parametru;
+	}
+	public int getRatePerHour() {
+		return ratePerHour;
+	}
+	public void setRatePerHour(int ratePerHour) {
+		this.ratePerHour = ratePerHour;
+	}
+	
+	
+	
+
+}
--- src/curs1/FirstClass.java
+++ src/curs1/FirstClass.java
@@ -1,10 +1,21 @@
 package curs1; // numele pachetului in care se afla clasa
 
 public class FirstClass { // numele pe care l-am dat clasei =>corpul clasei
+	
+	String nume = "Ioana"; // variabila de instanta, are nevoie de un obiect al clasei pt. a fi utilizata
+	
+	int varsta = 23;
 
 	public static void main(String[] args) {
 		System.out.println("TEST");
+		
+		FirstClass obiect = new FirstClass();
+		
+		String prenume = "Ana"; // variabila locala, accesata in interiorul metodei
+		
+		System.out.println(prenume);
+		System.out.println(obiect.nume);
 
 	}
-
+    
 }
