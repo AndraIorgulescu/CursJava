9cb65e1badf10afd5c7d741f492ceed6f2b99d81
Andra Iorgulescu <andravoicav@gmail.com>
2023-02-23 19:50:16 +0200

    first commit

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,10 @@
+package curs1; // numele pachetului in care se afla clasa
+
+public class FirstClass { // numele pe care l-am dat clasei =>corpul clasei
+
+	public static void main(String[] args) {
+		System.out.println("TEST");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Andra
+ *
+ */
+module CursJava {
+}
